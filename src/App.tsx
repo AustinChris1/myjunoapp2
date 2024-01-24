60b2b910411b8a6cd1d42343ad895f94b84fe5ea
AustinChris1 <austinchrisiwu@gmail.com>
2024-01-24 21:53:21 +0100

    got something working

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { type Doc, initJuno, setDoc } from "@junobuild/core-peer";
+
+type Record = {
+  hello: string;
+};
+
+export default function App() {
+  const [record, setRecord] = useState<Doc<Record> | undefined>(undefined);
+
+  useEffect(() => {
+    (async () =>
+      await initJuno({
+        satelliteId: "pdurl-fqaaa-aaaal-adlga-cai",
+      }))();
+  }, []);
+
+  const insert = async () => {
+    const doc = await setDoc({
+      collection: "demo",
+      doc: {
+        key: `my-key-${new Date().getTime()}`,
+        data: {
+          hello: "world",
+        },
+      },
+    });
+
+    setRecord(doc);
+  };
+
+  return (
+    <>
+      <button onClick={insert}>Insert a document</button>
+      {record !== undefined && <span>Key: {record.key}</span>}
+    </>
+  );
+}
